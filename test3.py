b4a36e264325ba7ba00b34e3efe0ab31562a0c32
ztxl <95172504+Zhengtxl@users.noreply.github.com>
2024-12-08 12:10:33 +0800

    将所有的照片大小设置为640
    不经yolo标签可准确标记,经yolo标签坐标存在偏移,epoches设置为200,batches设为16为最佳,F1检测结果为83%,基本上能框选出来,但准确率不高

---
+++ test3.py
@@ -0,0 +1,66 @@
+import os
+import shutil
+
+import cv2
+
+# 获得当前路径
+dir = r"E:\Python\yolo11\datasets\1\\"
+os.chdir(dir)
+target_dir = dir + "images1"
+label_dir = dir + "labels1"
+# 正常数据集放一起
+# if not os.path.exists(target_dir):
+#     os.makedirs(target_dir)
+# if not os.path.exists(label_dir):
+#     os.makedirs(label_dir)
+lists = os.listdir(dir)
+
+# 获取ok和ng的路径
+ok_dir, ng_dir = dir + lists[0], dir + lists[1]
+
+# 将ok数据集中的所有bmp照片放入img中
+for filename in os.listdir(ok_dir):
+    if filename.endswith('.bmp'):
+        source_file_path = os.path.join(ok_dir, filename)
+        source_image = cv2.imread(source_file_path)
+        source_image=cv2.resize(source_image,(640,640))
+        target_file_path = os.path.join(target_dir, filename)
+        # 复制文件到目标目录
+        cv2.imwrite(target_file_path,source_image)
+        label_name = label_dir + '\\' + (filename.split(' ')[0]).split('.')[0] + '.txt'
+        open(label_name, 'w').close()
+# 将不包含掩码图的ng照片放入img中，同时将有缺陷部分记录下来
+mark_txt = ng_dir + "\\mark.txt"
+
+with open(mark_txt, encoding='utf-8') as file:
+    lines = ''.join(file.readlines()).splitlines()
+    for line in lines:
+        # 获取路径
+        defect_name = ng_dir + '\\' + line.split(' ')[0]
+        mask_name = ng_dir + '\\' + line.split(' ')[1]
+        defect_image = cv2.imread(defect_name)
+        mask_image = cv2.imread(mask_name, cv2.IMREAD_GRAYSCALE)
+        defect_image = cv2.resize(defect_image, (640, 640))
+        mask_image = cv2.resize(mask_image, (640, 640))
+
+        img_x, img_y = defect_image.shape[0], defect_image.shape[1]
+        print(img_x, img_y, defect_name)
+        # 使用反转后的掩膜图提  取缺陷区域
+        defect_area = cv2.bitwise_and(defect_image, defect_image, mask=mask_image)
+        # 查找掩膜图中的轮廓
+        contours, _ = cv2.findContours(mask_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        target_dir_path = os.path.join(target_dir, line.split(' ')[0])
+        label_name = label_dir + '\\' + (line.split(' ')[0]).split('.')[0] + '.txt'
+        cv2.imwrite(target_dir_path, defect_image)
+        with open(label_name, 'w') as file:
+            for contour in contours:
+                # 计算轮廓的外接矩形
+                x, y, w, h = cv2.boundingRect(contour)
+                x_ratio = x / img_y
+                # 计算相对于图像尺寸的比例
+                y_ratio = y / img_x
+                w_ratio = w / img_y
+                h_ratio = h / img_x
+                print(x_ratio,y_ratio,w_ratio,h_ratio)
+                # 将信息格式化为字符串并写入文件
+                file.write(f"0 {x_ratio:.6f} {y_ratio:.6f} {w_ratio:.6f} {h_ratio:.6f}\n")
